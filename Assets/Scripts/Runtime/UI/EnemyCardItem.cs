4cea33bd14973be37876defc0e88843ccc00360d
longpeng.lin <935564550@qq.com>
2024-11-01 10:20:27 +0800

    怪物名

--- Assets/Scripts/Runtime/UI/EnemyCardItem.cs
+++ Assets/Scripts/Runtime/UI/EnemyCardItem.cs
@@ -13,11 +13,13 @@ namespace UI
         public int curHp;
 
         private Text _hpText;
+        private Text _nameText;
         private Image _icon;
 
         private void Awake()
         {
             _hpText = transform.Find("bg/hpText").GetComponent<Text>();
+            _nameText = transform.Find("bg/nameText").GetComponent<Text>();
             _icon = transform.Find("bg/icon").GetComponent<Image>();
         }
 
@@ -26,6 +28,7 @@ namespace UI
             _enemyConfig = cardConfig;
             curHp = maxHp = _enemyConfig.maxHp;
             _icon.sprite = Resources.Load<Sprite>(cardConfig.iconPath);
+            _nameText.text = _enemyConfig.enemyName;
             _hpText.text = $"{curHp.ToString()}/{maxHp.ToString()}";
         }
         
